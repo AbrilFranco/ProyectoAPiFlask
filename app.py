9c2816fd3ed425143b9da09af0c767225b9076dc
Abril Franco <abrifran2412@gmail.com>
2025-01-14 04:33:16 -0600

    corregido

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def view_notebook(filename):
     return render_template('notebook.html', notebook_html=body)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
